9a2a9810dccc2b1acc28427faf72c2b643dda6c3
Vladimir Dabić <dabic.vladimir@gmail.com>
2018-06-12 00:19:35 +0200

    polish

--- src/path-service.js
+++ src/path-service.js
@@ -1,15 +1,15 @@
 
-import * as _ from './helpers'
+import * as _v_ from './helpers'
 
 export const getPathArr = (order, val, path = [], result = []) => {
     // looping through the Array
     for (let [index, item] of order.entries()) {
         // if it's Object
-        if (_.isObj(item)) {
+        if (_v_.isObj(item)) {
             // push object's key
-            path.push(index, String(_.getObjKey(item)))
+            path.push(index, String(_v_.getObjKey(item)))
             // Recursion with the object Value, passing all parameters
-            getPathArr(_.getObjValue(item), val, path, result)
+            getPathArr(_v_.getObjValue(item), val, path, result)
         // if not an Object
         } else {
             // push index to the path Array
@@ -24,7 +24,7 @@ export const getPathArr = (order, val, path = [], result = []) => {
             }
         }
         // if current item is not string, splice last two elements, otherwise splice one element
-        !_.isStr(item) ? path.splice(-2, 2) : path.splice(-1, 1)
+        !_v_.isStr(item) ? path.splice(-2, 2) : path.splice(-1, 1)
     }
     return result
 }
@@ -33,12 +33,12 @@ export function getAllPathsStr (order, path = '', result = {}) {
     // looping through the Array
     for (let [index, item] of order.entries()) {
         // if it's Object
-        if (_.isObj(item)) {
+        if (_v_.isObj(item)) {
             // Concatenate to the path object key as a string
-            path += `[${index}]['${_.getObjKey(item)}']`
+            path += `[${index}]['${_v_.getObjKey(item)}']`
             // if objValue is Array then Recursion with the object Value, passing all parameters, otherwise make path
-            // _.isArr(_.getObjValue(item)) ? getAllPathsStr(_.getObjValue(item), path, result) : result[_.getObjValue(item)] = path
-            getAllPathsStr(_.getObjValue(item), path, result)
+            // _v_.isArr(_v_.getObjValue(item)) ? getAllPathsStr(_v_.getObjValue(item), path, result) : result[_v_.getObjValue(item)] = path
+            getAllPathsStr(_v_.getObjValue(item), path, result)
         //  if not an Object (string it is)
         } else {
             // concatenate index number as a string
@@ -47,10 +47,10 @@ export function getAllPathsStr (order, path = '', result = {}) {
             result[item] = path
         }
         // Declaring variable to store length of current Objects key
-        let num = _.getObjKey(item)[0].length
+        let num = _v_.getObjKey(item)[0].length
         // if current item is not string, trim path from the end by length of the previously added string + extra for the "[item]"
         // else, if's string just trim the "[item]"
-        !_.isStr(item) ? path = _.trimEnd(path, num, 7) : path = _.trimEnd(path, 0, 3)
+        !_v_.isStr(item) ? path = _v_.trimEnd(path, num, 7) : path = _v_.trimEnd(path, 0, 3)
     }
     return result
 }
